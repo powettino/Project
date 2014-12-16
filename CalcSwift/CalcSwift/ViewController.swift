49a8f8c9863d028911d09b8e1a9ff811c8cefa99
powettino <iacoperi@gmail.com>
2014-12-16 21:00:01 +0100

    tastiera

--- CalcSwift/CalcSwift/ViewController.swift
+++ CalcSwift/CalcSwift/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var LabelCalc: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func cliccato(sender: AnyObject) {
+        var name = sender.title as String!;
+        switch (name){
+        case "=":
+            LabelCalc.text = name;
+        case "+" :
+            LabelCalc.text = name;
+        case "-":
+            LabelCalc.text = name;
+        case "x":
+            LabelCalc.text = name;
+        case "/":
+            LabelCalc.text = name;
+        default:
+            LabelCalc.text = LabelCalc.text! + 	name;
+        }
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
