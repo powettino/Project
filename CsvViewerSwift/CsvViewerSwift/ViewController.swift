47a62e7be373b5c76d092554cba8d9f2f93da4d6
powettino <iacopo.peri@SM-A6F1P3.local>
2014-12-19 13:17:56 +0100

    animazione ok, da fare le barre

--- CsvViewerSwift/CsvViewerSwift/ViewController.swift
+++ CsvViewerSwift/CsvViewerSwift/ViewController.swift
@@ -19,7 +19,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var graph: UIView!
     let x = [1,2,3,4,5,6];
     let y = [1,2,3,4,5,6];
-
+    
+    var filePath:String? = "/Users/iacopo.peri/workspace/Project/CsvViewerSwift/CsvViewerSwift/punti.csv";
+    
+    @IBOutlet weak var tempor: UILabel!
     var coveringView:UIView!;
     
     override func viewDidLoad() {
@@ -31,27 +34,27 @@ class ViewController: UIViewController {
         roundLabel(numEl);
         roundLabel(numCol);
         roundLabel(filename);
-
-        coveringView = UIView(frame: CGRectMake(self.graph.frame.origin.x, self.graph.frame.origin.y, self.graph.frame.size.width, self.graph.frame.size.height));
+        
+        coveringView = UIView(frame: CGRectMake(0, 0, self.graph.frame.size.width, self.graph.frame.size.height));
         coveringView.backgroundColor = UIColor.blackColor();
         coveringView.alpha=0.0;
         
-        self.view.addSubview(coveringView);
+        self.graph.addSubview(coveringView);
         
+        UIView.animateWithDuration(0.8, animations: {self.coveringView.alpha=0.5;});
         
-//        coveringView = UIView  initWithFrame:CGRectMake(self.graphView.frame.origin.x
-//            ,self.graphView.frame.origin.y
-//            ,self.graphView.frame.size.width,self.graphView.frame.size.height)];
-//        [coveringDrawView setBackgroundColor:[UIColor blackColor]];
-//        [coveringDrawView setAlpha:0.0];
-//        
-//        [self.view addSubview:coveringDrawView];
-//        
-//        [UIView animateWithDuration:1 animations:^{
-//            [coveringDrawView setAlpha:0.5];
-//            }];
-
+        filename.text=filePath!;
         
+        if let data = splitData(splitLines(loadDati())) {
+            var somma = 0;
+            for (val1, val2) in data {
+                tempor.text = tempor.text! + "\n" + val1 + " - " + val2;
+                somma += val2.toInt()!;
+            }
+            
+            colonne.text = String(data.count);
+            elementi.text = String(somma);
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -59,6 +62,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func splitData(lines:[String]?)->[(String,String)]?{
+        if let values = lines {
+            var x:[String]=[String]();
+            var y:[String]=[String]();
+            var array:[(String, String)]=[(String, String)]();
+            for line in values {
+                array.append(line.componentsSeparatedByString(",")[0], line.componentsSeparatedByString(",")[1]);
+            }
+            return array;
+        }
+        return nil;
+    }
+    
+    func splitLines(loading:String?)->[String]?{
+        if let lines = loading {
+            return lines.componentsSeparatedByString("\n");
+        }
+        return nil;
+    }
     
     func roundLabel(label:UILabel){
         label.backgroundColor=UIColor.clearColor();
@@ -66,6 +88,14 @@ class ViewController: UIViewController {
         label.layer.cornerRadius = 5;
         label.layer.borderWidth=1.0;
         label.layer.borderColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 0.5).CGColor;
+    }
+    
+    func loadDati()->String?{
+        if let fp = self.filePath {
+            var lines = NSString(contentsOfFile: filePath!, encoding: NSUTF8StringEncoding, error: nil);
+            return lines;
+        }
+        return nil;
     }
     
     
