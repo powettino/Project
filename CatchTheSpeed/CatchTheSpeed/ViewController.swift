d88714d068feddc9a9883632e7ace4d93c3f6029
powettino <iacopo@Iacopos-Mac-Pro.local>
2015-01-19 00:47:30 +0100

    la modalità stressing è quasi finita manca l'aggiornamento del punteggio

--- CatchTheSpeed/CatchTheSpeed/ViewController.swift
+++ CatchTheSpeed/CatchTheSpeed/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var labelCount: UILabel!
     @IBOutlet weak var labelCongrats: UILabel!
     @IBOutlet weak var fadingView: UIView!
     @IBOutlet weak var startButton: UIButton!
@@ -37,6 +38,8 @@ class ViewController: UIViewController {
     var level : Int = 1
     var minAngle: CGFloat = 0
     var maxAngle : CGFloat = 0
+    var counter = 3;
+    var timerEndGame = NSTimer()
     
     
     @IBOutlet weak var acceleratorView: AcceleratorView!
@@ -46,6 +49,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.calcAngleOnLevel()
         self.fadingView.hidden=true
+        self.labelCongrats.hidden=true
+        self.labelCount.hidden=true
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -72,43 +77,74 @@ class ViewController: UIViewController {
     @IBAction func startGame(sender: AnyObject) {
         switch started  {
         case false:
+            self.counter=3
             self.startButton.setTitle(buttonLabel.stop.rawValue, forState: UIControlState.Normal)
             self.timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: Selector("updateGame"), userInfo: nil, repeats: true)
             started = !started
         default:
             switch mod {
+            case modalita.stressing:
+                self.counter=1
             case modalita.soft:
                 self.startButton.setTitle(buttonLabel.start.rawValue, forState: UIControlState.Normal)
                 self.timer.invalidate()
                 started = !started
                 var stoppedAngle = self.acceleratorView.getTickerAngle()
                 if (stoppedAngle >= self.minAngle && stoppedAngle <= self.maxAngle ){
-//                    var timerEndGame = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("messageGame"), userInfo: nil, repeats: false)
                     
                     var origFrame = self.fadingView.frame
                     self.fadingView.frame = CGRectMake(origFrame.origin.x, origFrame.origin.y, origFrame.width, 0)
-                    
-                    UIView.animateWithDuration(1, animations: {
+                    self.level++
+                    UIView.animateWithDuration(0.5, animations: {
                         self.fadingView.frame = origFrame
-                        
                         self.fadingView.alpha = 0.7
                         self.fadingView.hidden = false
-                        }, completion: {
-                            
+                        self.levelText.text = String(self.level)
+                        }, completion: { finished in
+                            UIView.animateWithDuration(1, animations: {
+                                self.labelCount.hidden = false
+                                self.labelCongrats.hidden = false}, completion: {
+                                    finished in
+                                    self.timerEndGame = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("messageGame"), userInfo: nil, repeats: true)
+                                }
+                            )
                         }
                     )
                     
-                    
                 }else{
-                    
+                    var alert = UIAlertController(title: "OH NO!!!", message: "Omg, you have lost! LOSER!", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "F**K", style: UIAlertActionStyle.Destructive, handler: {finished in
+                        self.resetGame()
+                        self.level=1
+                        self.levelText.text = String(self.level)
+                    }))
+                    self.presentViewController(alert, animated: true, completion: {})
                 }
-                self.resetGame()
             default :
                 started = !started
             }
             
             
         }
+    }
+    
+    func messageGame(){
+        println("uff \(self.counter)")
+        if(self.counter == 1){
+            
+            self.timerEndGame.invalidate()
+            UIView.animateWithDuration(0.2, animations: {
+                
+                self.fadingView.alpha = 0
+                self.fadingView.hidden = false}, completion: {finished in
+                    self.counter=3
+                    self.labelCount.text = String(self.counter)
+                }
+            )
+        }else{
+            self.counter -= 1
+        }
+        self.labelCount.text = String(self.counter)
     }
     
     private func calcAngleOnLevel(){
