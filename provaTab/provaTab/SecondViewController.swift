6fa4f18cf9cb43ee8982942d374bb5d79cb508da
powettino <iacoperi@gmail.com>
2015-05-12 03:13:07 +0200

    aggiunto lo storyboard

---
+++ provaTab/provaTab/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  provaTab
+//
+//  Created by Iacopo Peri on 11/05/15.
+//  Copyright (c) 2015 Iacopo Peri. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
