733773eb7766f0e4515599c408f486a0878691f3
powettino <iacoperi@gmail.com>
2015-08-07 01:58:14 +0200

    aggiunto parse al progetto

--- CatchTheSpeed/CatchTheSpeed/AppDelegate.swift
+++ CatchTheSpeed/CatchTheSpeed/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("7Zn8mV9WPMzBprhPiZDcqOVxlGc6UYNpmTN4qQLs", clientKey: "dpjjRbdwsfCO1wHYWuSNXjXsopNB57dl8EHlp2Fe")
+        PFAnalytics.trackAppOpenedWithLaunchOptionsInBackground(launchOptions, block: (nil))
         return true
     }
 
